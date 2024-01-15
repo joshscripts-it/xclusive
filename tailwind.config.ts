77cbfb7581571d2daf183675b7f45f402074f16d
Joshag <tech.gems.biz@gmail.com>
2024-01-15 16:57:46 +0100

    important updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        playStation: "url('../assets/images/ps5.png')",
+        womenCollection: 'url("../assets/images/women_wear.png")',
+        speakers: 'url("../assets/images/speakers.png")',
+        perfums: 'url("../assets/images/perfums.png")',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class",
+
+  plugins: [require("@tailwindcss/forms"), nextui({ addCommonColors: true })],
+};
+
+export default config;
